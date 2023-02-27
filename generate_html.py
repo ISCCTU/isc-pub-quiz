1b7b3563d013cc05f360dade1acdadd2e0f4e08b
Dominik Hodan <hodandom@fel.cvut.cz>
2023-02-27 09:09:44 +0100

    clean-up the Python code

--- generate_html.py
+++ generate_html.py
@@ -1,53 +1,60 @@
+import sys
+
 from jinja2 import Environment, FileSystemLoader
-from typing import Optional, List
+from typing import Optional, List, Dict, Mapping, Any
 
 import random
 import yaml
 
-config = "example.yaml"
-with open(config, 'r', encoding='utf8') as fp:
-    quiz_config = yaml.safe_load(fp)
 
-template=Environment(loader=FileSystemLoader("./slides")).get_template(quiz_config["template"])
-
-def shuffle_choices(answer,wrong):
+def shuffle_choices(answer: str, wrong: List[str]):
     # generate a random order and remember the correct answer
     num_options = len(wrong) + 1  # +1 for answer
     correct_idx = random.randint(0, num_options - 1)
     random.shuffle(wrong)
     wrong.insert(correct_idx, answer)
-    choices=wrong
-    return choices,correct_idx
+    choices = wrong
+    return choices, correct_idx
 
 
-def parse_question(topic,question,question_idx):
-    question["title"]=f"{topic['name']}: Q{question_idx+1}"
-    if question["type"]=="simple":
+def parse_question(topic: Dict[str, Any], question: Dict[str, Any], question_idx: int) -> Dict[str, Any]:
+    question["title"] = f"{topic['name']}: Q{question_idx + 1}"
+    if question["type"] == "simple":
         pass
-    elif question["type"]=="choice":
-        choices,correct_idx=shuffle_choices(question["answer"],question["wrong"])
-        question["choices"]=choices
-        question["correct_idx"]=correct_idx
+    elif question["type"] == "choice":
+        choices, correct_idx = shuffle_choices(question["answer"], question["wrong"])
+        question["choices"] = choices
+        question["correct_idx"] = correct_idx
     return question
 
-quiz={
-    "date": quiz_config["date"],
-    "place": quiz_config["place"],
-    "blocks": [],
-}
-for t,topic in enumerate(quiz_config["topics"]):
-    if t%quiz_config["topics_per_block"]==0:
-        quiz["blocks"].append([])
-    parsed_questions=[]
-    for q,question in enumerate(topic["questions"]):
-        parsed_questions.append(parse_question(topic,question,q))
-    topic["questions"]=parsed_questions
-    quiz["blocks"][-1].append(topic)
 
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        config = sys.argv[1]
+    else:
+        config = "example.yaml"
 
-# quiz["blocks"]=[[quiz["topics"][i+j*quiz["topics_per_block"]] for i in range(quiz["topics_per_block"])] for j in range(len(quiz["topics"])//quiz["topics_per_block"])]
-print(quiz)
-content=template.render(quiz)
+    with open(config, 'r', encoding='utf8') as fp:
+        quiz_config = yaml.safe_load(fp)
 
-with open("output.html", "w",encoding="utf-8") as fh:
-    fh.write(content)
+    # parse the quiz for templating
+    quiz = {
+        "date": quiz_config["date"],
+        "place": quiz_config["place"],
+        "blocks": [],
+    }
+    for t, topic in enumerate(quiz_config["topics"]):
+        if t % quiz_config["topics_per_block"] == 0:
+            quiz["blocks"].append([])
+        parsed_questions = []
+        for q, question in enumerate(topic["questions"]):
+            parsed_questions.append(parse_question(topic, question, q))
+        topic["questions"] = parsed_questions
+        quiz["blocks"][-1].append(topic)
+
+    # generate the web page from the template
+    template = Environment(loader=FileSystemLoader("./slides")).get_template(quiz_config["template"])
+    content = template.render(quiz)
+
+    with open(quiz_config["output"], "w", encoding="utf-8") as fh:
+        fh.write(content)
