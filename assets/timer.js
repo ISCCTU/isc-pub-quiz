91d4c2a2f7d4957fe11da274bd5e942c75176128
Dominik Hodan <domho108@gmail.com>
2023-11-26 15:39:01 +0100

    much more sensible timers

--- assets/timer.js
+++ assets/timer.js
@@ -1,46 +1,19 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let timer;
-    let time = 60; // 60 seconds = 1 minute
+function countdownText(time) {
+    if (time > 0) 
+        return `${Math.floor(time / 60)}:${(time % 60).toString().padStart(2, '0')}`;
+    else
+        return "BREAK";
+}
 
-    function updateCountdown() {
-        const countdown = document.querySelectorAll(".countdown");
-        for (let i = 0; i < countdown.length; i++) {
-            if (time > 0) {
-                countdown[i].innerText = `${Math.floor(time / 60)}:${(time % 60).toString().padStart(2, '0')}`
-            } else {
-                countdown[i].innerText = 'BREAK'
-            }
+function startTimer(element, start_time) {
+    time = start_time;
+    timer = setInterval(function () {
+        if (time > 0) {
+            time--;
+            text=countdownText(time);
+            element.innerText=text;
+        } else {
+            clearInterval(timer);
         }
-    }
-
-    function startTimer() {
-        if (timer) clearInterval(timer);
-        timer = setInterval(function () {
-            if (time > 0) {
-                time--;
-                updateCountdown();
-            } else {
-                clearInterval(timer);
-            }
-        }, 1000);
-    }
-
-    function resetTimer() {
-        time = 60;
-        updateCountdown();
-        clearInterval(timer);
-    }
-
-    function startOrRestartTimerOnKeyPress(e) {
-        if (e.key === " " || e.key === "ArrowRight") {
-            resetTimer();
-            startTimer();
-        }
-    }
-
-    document.addEventListener("keydown", (e) => {
-        startOrRestartTimerOnKeyPress(e)
-    });
-    document.getElementById("startButton").addEventListener("click", startTimer);
-    document.getElementById("resetButton").addEventListener("click", resetTimer);
-});
+    }, 1000);
+}
