0b41a4ef8880149e42f83c36a1552592184aa71c
Dominik Hodan <domho108@gmail.com>
2023-12-01 17:49:27 +0100

    fix multiple timer regression

--- assets/timer.js
+++ assets/timer.js
@@ -1,15 +1,16 @@
 function startTimer(element, start_time) {
+    if (element.getAttribute("data-used") == "true") {
+        // avoid running the timer multiple times
+        return;
+    } else {
+        element.setAttribute("data-used", "true");
+    }
     time = start_time;
     background = element.parentNode;
     bg_final_size = 3;
     fill_scale = background.parentNode.offsetWidth / element.offsetWidth;
-    console.log(fill_scale);
     ticking_transition = "all 0.8s cubic-bezier(.2, 15, 0, .61)";
     end_transition = "all 5s cubic-bezier(.45,.05,.55,.95)";
-    if (element.getAttribute("data-used") === "true") {
-        // avoid running the timer multiple times
-        return;
-    }
     timer = setInterval(function () {
         if (time > 0) {
             element.innerText = `${Math.floor(time / 60)}:${(time % 60).toString().padStart(2, '0')}`;
@@ -26,7 +27,6 @@ function startTimer(element, start_time) {
             background.style.transform = "scale(" + scale + ")";
             background.style.borderRadius = 0;
             element.style.transform = "scale(" + 1 / scale + ")";
-            element.setAttribute("data-used", "true");
         }
     }, 1000);
 
